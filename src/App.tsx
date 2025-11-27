5938f53011816ee359b48e09d4251f5f0d7690c6
josh <joshzhaa@gmail.com>
2025-11-27 03:08:03 +0000

    add some components, scaffolding, and formatting with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import { Item } from "@/components/ui/item";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Hexadle</h1>
       <div>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
+        <Item> </Item>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
