3ef56da28ce7acb622c3f482239f213b754c0a6e
josh <joshzhaa@gmail.com>
2025-12-14 08:19:05 +0000

    npm run fmt

--- src/features/game.tsx
+++ src/features/game.tsx
@@ -1,39 +1,57 @@
 import { useEffect, useState } from "react";
 import { GuessTable } from "@/features/guess-table";
 
-const AllowedInputs = new Set([ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" ])
+const AllowedInputs = new Set([
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+]);
 
 function Game() {
-  const [guesses, setGuesses] = useState([""])
+  const [guesses, setGuesses] = useState([""]);
 
   const handleKey = (event: KeyboardEvent) => {
-    console.log(event.key)
-    const key = event.key.toUpperCase()
+    console.log(event.key);
+    const key = event.key.toUpperCase();
     if (!(key in AllowedInputs)) {
-      return
+      return;
     }
     // INVARIANT: guesses.length >= 1
-    let last = guesses[guesses.length - 1]
+    let last = guesses[guesses.length - 1];
     if (last.length < 6) {
-      last += key
+      last += key;
     } else {
-
     }
-  }
+  };
 
   const listenEvents = () => {
-    window.addEventListener("keydown", handleKey)
+    window.addEventListener("keydown", handleKey);
     return () => {
-      window.removeEventListener("keydown", handleKey)
-    }
-  }
+      window.removeEventListener("keydown", handleKey);
+    };
+  };
 
-  useEffect(listenEvents, [handleKey])
-  return <>
+  useEffect(listenEvents, [handleKey]);
+  return (
+    <>
       <div className="flex">
-        <GuessTable className="w-64" colors={guesses}/>
+        <GuessTable className="w-64" colors={guesses} />
       </div>
-  </>
+    </>
+  );
 }
 
-export { Game }
+export { Game };
--- src/features/guess-table.tsx
+++ src/features/guess-table.tsx
@@ -2,44 +2,55 @@ import { Table, TableBody, TableRow, TableCell } from "@/components/ui/table";
 import { type ComponentProps } from "react";
 
 interface GuessTableProps extends ComponentProps<"table"> {
-  colors: string[]
+  colors: string[];
 }
 
 function GuessTable({ colors, className, ...props }: GuessTableProps) {
-  const populateRow = (color: string) => <GuessRow color={color}/>
+  const populateRow = (color: string) => <GuessRow color={color} />;
   return (
-    <Table
-      className={className}
-      {...props}
-    >
-      <TableBody>
-        { colors.map(populateRow) }
-      </TableBody>
+    <Table className={className} {...props}>
+      <TableBody>{colors.map(populateRow)}</TableBody>
     </Table>
   );
 }
 
 interface GuessRowProps extends ComponentProps<"tr"> {
-  color: string
+  color: string;
 }
 
 function GuessRow({ color, className, ...props }: GuessRowProps) {
-  const populateCell = (char: string) => <GuessCell char={char as HexChar}/>
+  const populateCell = (char: string) => <GuessCell char={char as HexChar} />;
   return (
     <TableRow className={className} {...props}>
-      { color.split("").map(populateCell) }
+      {color.split("").map(populateCell)}
     </TableRow>
   );
 }
 
-type HexChar = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "A" | "B" | "C" | "D" | "E" | "F"
+type HexChar =
+  | "0"
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F";
 
 interface GuessCellProps extends ComponentProps<"td"> {
-  char: HexChar
+  char: HexChar;
 }
 
-function GuessCell({char, ...props}: GuessCellProps) {
-  return <TableCell>{char}</TableCell>
+function GuessCell({ char, ...props }: GuessCellProps) {
+  return <TableCell>{char}</TableCell>;
 }
 
 export { GuessTable, GuessRow, GuessCell };
