516ac35aa962ede21b1416fb6e5d7852d4cc5af7
josh <joshzhaa@gmail.com>
2025-12-14 07:13:31 +0000

    guess-table.tsx is "complete"

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { GuessTable } from "@/features/guess-table";
 import { ColorBlock } from "@/components/color-block";
 
 function App() {
+  const guesses = ["124456", "1234AF", "654321"]
   return (
     <>
       <h1>Hexadle</h1>
       <div className="flex">
-        <GuessTable className="w-64" />
+        <GuessTable className="w-64" colors={guesses}/>
       </div>
       <ColorBlock color="123456" className="w-64 h-64" />
       <ColorBlock color="523456" className="w-64 h-64" />
