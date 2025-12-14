66fa475291d8275fe2d8e6faf6e3a15412ab680c
josh <joshzhaa@gmail.com>
2025-12-14 08:18:36 +0000

    game wip: evaluate correctness, and show color block

--- src/features/game.tsx
+++ src/features/game.tsx
@@ -1,11 +1,24 @@
-import { GuessTable } from "./guess-table";
 import { useEffect, useState } from "react";
+import { GuessTable } from "@/features/guess-table";
+
+const AllowedInputs = new Set([ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" ])
 
 function Game() {
-  const [guesses, setGuesses] = useState(["124456", "1234AF", "654321"])
+  const [guesses, setGuesses] = useState([""])
 
   const handleKey = (event: KeyboardEvent) => {
     console.log(event.key)
+    const key = event.key.toUpperCase()
+    if (!(key in AllowedInputs)) {
+      return
+    }
+    // INVARIANT: guesses.length >= 1
+    let last = guesses[guesses.length - 1]
+    if (last.length < 6) {
+      last += key
+    } else {
+
+    }
   }
 
   const listenEvents = () => {
--- src/features/guess-table.tsx
+++ src/features/guess-table.tsx
@@ -32,7 +32,7 @@ function GuessRow({ color, className, ...props }: GuessRowProps) {
   );
 }
 
-type HexChar = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "A" | "B" | "C" | "D" | "E" | "F"
+type HexChar = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "A" | "B" | "C" | "D" | "E" | "F"
 
 interface GuessCellProps extends ComponentProps<"td"> {
   char: HexChar
