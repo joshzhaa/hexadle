c6a28d4cfd5d34af4daf8c63d1eca3202cbf8b53
jz <joshzhaa@gmail.com>
2026-01-24 18:59:20 -0800

    fix bug that gives wrong evaluations

--- src/lib/evaluate-guess.ts
+++ src/lib/evaluate-guess.ts
@@ -5,16 +5,31 @@ const WHITE = "FFFFFF";
 // Compares guess to target, returns appropriate background colors
 function evaluate(guess: string[], target: string[]): string[] {
   const targetCharCounts = new Map();
-  for (const char of target) {
-    targetCharCounts.set(char, (targetCharCounts.get(char) || 0) + 1);
+
+  const backgrounds = Array(6).fill(WHITE)
+  // 1st pass colors greens
+  for (let i = 0; i < 6; i++) {
+    if (guess[i] == target[i]) {
+      backgrounds[i] = GREEN
+    } else {
+      // count up char counts for second pass
+      const char = target[i]
+      targetCharCounts.set(char, (targetCharCounts.get(char) || 0) + 1);
+    }
   }
 
-  return guess.map((char: string, i: number) => {
-    if (!targetCharCounts.has(char) || targetCharCounts.get(char) <= 0)
-      return WHITE;
-    targetCharCounts.set(char, targetCharCounts.get(char) - 1);
-    return char == target[i] ? GREEN : YELLOW;
-  });
+  // 2nd pass colors yellows
+  for (let i = 0; i < 6; i++) {
+    if (backgrounds[i] == GREEN) continue;
+
+    const char = guess[i]
+    if (targetCharCounts.has(char) && targetCharCounts.get(char) > 0) {
+      backgrounds[i] = YELLOW
+      targetCharCounts.set(char, targetCharCounts.get(char) - 1)
+    }
+  }
+
+  return backgrounds;
 }
 
 export { evaluate };
